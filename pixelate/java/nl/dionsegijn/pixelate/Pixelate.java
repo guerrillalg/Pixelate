c14e57d039194116f1ce0abf2c9623a58b715c83
Dion Segijn <dion@blendle.com>
2017-02-06 22:03:08 +0100

    Version 1.1.0

--- pixelate/java/nl/dionsegijn/pixelate/Pixelate.java
+++ pixelate/java/nl/dionsegijn/pixelate/Pixelate.java
@@ -1,174 +1,84 @@
 package nl.dionsegijn.pixelate;
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Build;
-import android.util.AttributeSet;
+import android.support.annotation.NonNull;
 import android.widget.ImageView;
 
-import nl.dionsegijn.differences.R;
+import static android.graphics.Bitmap.Config.ARGB_8888;
 
-public class Pixelate extends ImageView {
+public class Pixelate implements OnPixelateListener {
 
-    private boolean clearCanvas = false;
-    private boolean isTouched = false;
-    private boolean render = false;
+    private int density = 12;
+    private OnPixelateListener onPixelateListener;
+    private PixelateThread pixelateThread;
+    private int x, y, width, height = -1;
 
-    private int cols;
-    public Bitmap drawingCache;
-    private Paint paint = new Paint();
+    private Bitmap destBitmap;
+    private ImageView targetImageView;
+    private boolean loadIntoImageView = true;
 
-    /* Properties for drawing just a certain area on the canvas */
-    private int touchedX;
-    private int touchedY;
-    private int touchedSize;
-
-    public Pixelate(Context context) {
-        super(context);
-        setDrawingCacheEnabled(true);
+    public Pixelate(@NonNull Bitmap bitmap) {
+        pixelateThread = new PixelateThread();
+        setBitmap(bitmap);
     }
 
-    public Pixelate(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        setDrawingCacheEnabled(true);
-        readAttribute(context, attrs);
+    public Pixelate(@NonNull final ImageView imageView) {
+        pixelateThread = new PixelateThread();
+        this.targetImageView = imageView;
+        imageView.setDrawingCacheEnabled(true);
+        setBitmap(imageView.getDrawingCache());
     }
 
-    public Pixelate(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        setDrawingCacheEnabled(true);
-        readAttribute(context, attrs);
+    public Pixelate setShouldHandleImageView(boolean loadIntoImageView) {
+        this.loadIntoImageView = loadIntoImageView;
+        return this;
     }
 
-    private void readAttribute(Context context, AttributeSet attrs) {
-        TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.Pixelate,
-                0, 0);
-        int density = a.getInteger(R.styleable.Pixelate_density, 0);
-        a.recycle();
-        pixelate(density);
+    public Pixelate setDensity(int density) {
+        this.density = density;
+        return this;
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public Pixelate(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        setDrawingCacheEnabled(true);
+    public Pixelate setListener(OnPixelateListener listener) {
+        this.onPixelateListener = listener;
+        return this;
+    }
+
+    public Pixelate setArea(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    public Pixelate setBitmap(Bitmap bitmap) {
+        // TODO instead of copying whole bitmap, draw on new one
+        this.destBitmap = bitmap.copy(ARGB_8888, true);
+        return this;
+    }
+
+    public void make() {
+        if(pixelateThread == null || pixelateThread.isRendering()) {
+            // Create a new thread when the old one is busy
+            pixelateThread = new PixelateThread();
+        }
+        pixelateThread.setBitmap(this.destBitmap);
+        pixelateThread.pixelate(density);
+        if(x != -1 && y != -1) {
+            pixelateThread.setArea(x, y, width, height);
+        }
+        // Let this class handle the callback and not the thread
+        pixelateThread.setPixelateListener(this);
+        pixelateThread.start();
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        if(clearCanvas) {
-            canvas.drawColor(Color.TRANSPARENT);
-            clearCanvas = false;
-            return;
+    public void onPixelated(final Bitmap bitmap, final int density) {
+        if(loadIntoImageView && targetImageView != null){
+            targetImageView.setImageBitmap(bitmap);
         }
-
-        if(isTouched) {
-            renderPixels(canvas);
-            isTouched = false;
-            return;
-        }
-        renderPixels(canvas);
+        if(onPixelateListener != null) onPixelateListener.onPixelated(bitmap, density);
     }
 
-    public void clear() {
-        clearCanvas = true;
-        invalidate();
-    }
-
-    public void pixelate(int cols) {
-        if(cols == 0) return;
-        this.cols = cols;
-        render = true;
-        this.invalidate();
-    }
-
-    public void pixelateArea(int x, int y, int size, int density) {
-        cols = density;
-        touchedX = x;
-        touchedY = y;
-        touchedSize = size;
-        isTouched = true;
-        this.invalidate();
-    }
-
-    private void renderPixels(Canvas canvas) {
-        if(!render && !isTouched) return;
-        if(cols < 1) cols = 1;
-
-        Bitmap bitmap = null;
-        if(isDrawingCacheEnabled()) {
-            bitmap = Bitmap.createBitmap(getDrawingCache());
-            if(drawingCache == null) drawingCache = bitmap;
-            else bitmap = drawingCache;
-        } else {
-            bitmap = ((BitmapDrawable)getDrawable()).getBitmap();
-        }
-
-        if(bitmap == null) throw new NullPointerException("View does not contain image");
-
-        // get width and height of the entire bitmap
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-
-        int blockSize = 0;
-        int startX = 0;
-        int startY = 0;
-        double rows = 0;
-        if(isTouched) {
-            blockSize = touchedSize / cols;
-            startX = touchedX - (touchedSize / 2);
-            startY = touchedY - (touchedSize / 2);
-            rows = Math.round((double)touchedSize / (double)blockSize);
-        } else {
-            blockSize = width / cols;
-            rows = Math.round((double)height / (double)blockSize);
-            int mod = height % blockSize;
-            blockSize = blockSize + (mod/cols);
-        }
-
-        for (int row = 0; row < rows; row++ ) {
-
-            for (int col = 0; col < cols; col++ ) {
-                int midY = (blockSize) * row + startY;
-                int midX = (blockSize) * col + startX;
-
-                if(midX > width)
-                    return;
-                if(midY > height)
-                    return;
-
-                int pixel = 0;
-                try {
-                    pixel = bitmap.getPixel(midX, midY);
-                } catch(Exception ex) {
-                    return;
-                }
-
-                int r = Color.red(pixel);
-                int b = Color.blue(pixel);
-                int g = Color.green(pixel);
-                int a = Color.alpha(pixel);
-
-                paint.setARGB(a, r, g, b);
-
-                int left = (blockSize * row) + startY;
-                int top = (blockSize * col) + startX;
-                int right = ((blockSize * row) + blockSize) + startY;
-                int bottom = ((blockSize * col) + blockSize) + startX;
-
-                canvas.drawRect(top, left, bottom, right, paint);
-            }
-        }
-        render = false;
-    }
 }
